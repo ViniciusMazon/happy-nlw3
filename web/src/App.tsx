b9d3e19734336dfb919539ff2f8ca4cb811f67a3
Vinicius Mazon <vnpmazon@gmail.com>
2020-10-12 11:28:58 -0300

    feat(web): Adds Landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+import './styles/global.css';
+import './styles/pages/landing.css';
+import logoimg from './images/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Let's hack</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoimg} alt="Logo Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mudno</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Barreiras</strong>
+          <span>Bahia</span>
+        </div>
+
+        <a href="//#region " className="enter-app">
+          <FiArrowRight size={32} color="#8D734B" />
+        </a>
+      </div>
     </div>
   );
 }
