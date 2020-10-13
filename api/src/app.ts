5a26245c3e6f4641e16cf1366fb4775cbfd37a80
Vinicius Mazon <vnpmazon@gmail.com>
2020-10-13 09:43:35 -0300

    feat(api): Adds typeorm config

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,6 +2,8 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 
+import './database/connection';
+
 const app = express();
 import routes from './routes';
 
---
+++ api/src/database/migrations/1602592900408-create_orphanages.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createOrphanages1602592900408 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
