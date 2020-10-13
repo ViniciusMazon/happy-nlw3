e234a1fa204e01537eb21a9c54e7cf9cd6d6094f
Vinicius Mazon <vnpmazon@gmail.com>
2020-10-13 10:16:16 -0300

    feat(api): Adds table and model Orphanage

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,7 +1,34 @@
-import {request, response, Router} from 'express'
+import { request, response, Router } from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './app/models/Orphanage';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {response.json({ok: true})});
+routes.post('/orphanages', async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = request.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+
+  await orphanagesRepository.save(orphanage);
+  response.status(201).send(orphanage);
+});
 
 export default routes;
