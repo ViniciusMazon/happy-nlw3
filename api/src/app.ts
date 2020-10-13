a482181e118079e9866df25b660eb3e42dd9a450
Vinicius Mazon <vnpmazon@gmail.com>
2020-10-13 11:35:06 -0300

    feat(api): Adds exception handling

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
+import 'express-async-errors';
 import path from 'path';
 import cors from 'cors';
 
 import './database/connection';
+import errorHandler from './errors/handler';
 
 const app = express();
 import routes from './routes';
@@ -12,5 +14,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler)
 
 export default app;
